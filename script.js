f9d3349a9111cba4a31220d8b6b1fe818a4a1403
Donovan K <7donovan7@gmail.com>
2021-09-24 20:11:24 -0400

    polished timeblocks and added color coding

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-
+ var timeArray = ['.nine', '.ten', '.eleven', '.twelve', '.thirteen', '.fourteen', '.fifteen', '.sixteen', '.seventeen'];
 var a = $('#currentDay');
-
+var hour = 9;
 setInterval( function() {
     var timeEl = moment().format('MMMM Do YYYY, h:mm:ss a');
     a.text(timeEl);
   }, 1000);
+
+  for(var i=0; i < 9; i++) {
+      var check = moment().hour();
+      
+      if (check < hour) {
+        $(timeArray[i]).css({"background-color": "#77dd77", "color": "white"});
+       
+      } else if (check == hour) {
+         $(timeArray[i]).css({"background-color": "#ff6961", "color": "white"});
+      } else {
+         $(timeArray[i]).css({"background-color": "#d3d3d3", "color": "white"});
+      }
+      hour++;
+  }
