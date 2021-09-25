87668cd6ff079c559f99399b1c12b7431c90e86e
Donovan K <7donovan7@gmail.com>
2021-09-24 23:27:12 -0400

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # work-day-planner
+
+This program is suppose to be a planner for a typical 9 to 5 workday. 
+
+Here is the link:  https://donk1557.github.io/work-day-planner/
+
+## Header
+
+The top of the page displays the current date and time in {month day, year} and {hour: minutes: seconds} respectively.
+
+## Timeblocks
+
+There is one of these for every hour between 9 AM and 5 PM. The left-hand side shows the particular hour while the middle section is color-coded to reflect hours that have already passed (in grey), the current hour (in red), and hours yet to come (in green). Users can click on the middle section and type any events or notes they would like to recall. The user can then record their response by clicking the blue save button on the right-hand side. The users notes will persist on the page no matter how many times it is refreshed.
+
+
--- script.js
+++ script.js
@@ -1,35 +1,40 @@
- var timeArray = ['.nine', '.ten', '.eleven', '.twelve', '.thirteen', '.fourteen', '.fifteen', '.sixteen', '.seventeen'];
-var a = $('#currentDay');
+var timeArray = [
+  ".nine",
+  ".ten",
+  ".eleven",
+  ".twelve",
+  ".thirteen",
+  ".fourteen",
+  ".fifteen",
+  ".sixteen",
+  ".seventeen",
+];
+var a = $("#currentDay");
 var hour = 9;
-setInterval( function() {
-    var timeEl = moment().format('MMMM Do YYYY, h:mm:ss a');
-    a.text(timeEl);
-  }, 1000);
+setInterval(function () {               //time function to display current date & time
+  var timeEl = moment().format("MMMM Do YYYY, h:mm:ss a");
+  a.text(timeEl);
+}, 1000);
 
-  for(var i=0; i < 9; i++) {
-      var check = moment().hour();
-      
-      if (check < hour) {
-        $(timeArray[i]).css({"background-color": "#77dd77", "color": "white"});
-       
-      } else if (check == hour) {
-         $(timeArray[i]).css({"background-color": "#ff6961", "color": "white"});
-      } else {
-         $(timeArray[i]).css({"background-color": "#d3d3d3", "color": "white"});
-      }
-      hour++;
+for (var i = 0; i < 9; i++) {        //color-coder for each timeblock; compares the current hour with hour variable, colors accordingly, and appends hour variable by one
+  var check = moment().hour();
+
+  if (check < hour) {
+    $(timeArray[i]).css({ "background-color": "#77dd77", color: "white" });
+  } else if (check == hour) {
+    $(timeArray[i]).css({ "background-color": "#ff6961", color: "white" });
+  } else {
+    $(timeArray[i]).css({ "background-color": "#d3d3d3", color: "white" });
   }
-     var vent = '';
-   function addIn() {
-     var ev = $('.textarea');
-     vent = ev.val().trim();
-     localStorage.setItem('eve', JSON.stringify(vent));
-     $('.textarea').append(JSON.parse(localStorage.getItem('eve')));
-   }
-     var save = $('.saveBtn');
-     save.on('click', addIn); 
-     console.log(vent);
-     
-     
-    
-     
+  hour++;
+}
+var vent = "";
+function addIn() {            //records text in timeblock and saves it in local storage
+  var ev = $(".textarea");
+  vent = ev.val().trim();
+  localStorage.setItem("eve", JSON.stringify(vent));
+  $(".textarea").append(JSON.parse(localStorage.getItem("eve")));
+}
+var save = $(".saveBtn");    
+save.on("click", addIn);    //activates save button
+console.log(vent);
