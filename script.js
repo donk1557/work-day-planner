6679da1212578f7cd4c0f7d98566a4a00fe0ca3d
Donovan K <7donovan7@gmail.com>
2021-09-29 01:49:45 -0400

    fixed issues

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ var timeArray = [
 ];
 var a = $("#currentDay");
 var hour = 9;
+
+
 setInterval(function () {               //time function to display current date & time
   var timeEl = moment().format("MMMM Do YYYY, h:mm:ss a");
   a.text(timeEl);
@@ -30,11 +32,15 @@ for (var i = 0; i < 9; i++) {        //color-coder for each timeblock; compares 
 }
 var vent = "";
 function addIn() {            //records text in timeblock and saves it in local storage
-  var ev = $(".textarea");
+ for(i=0; i < 9; i++) {
+  var ev = $(timeArray[i]);
   vent = ev.val().trim();
-  localStorage.setItem("eve", JSON.stringify(vent));
-  $(".textarea").append(JSON.parse(localStorage.getItem("eve")));
+  localStorage.setItem(i, JSON.stringify(vent));
+ }
 }
 var save = $(".saveBtn");    
 save.on("click", addIn);    //activates save button
-console.log(vent);
+
+for(i=0; i < 9; i++) {
+$(timeArray[i]).val(JSON.parse(localStorage.getItem(i)));   //displays saved events
+}
